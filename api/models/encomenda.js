2b6b9b0247f2e669586df25042e8ac9de5b6a804
joaoleocadio <joaocarvalho388@gmail.com>
2020-06-30 18:08:55 +0100

    fix enum

--- api/models/encomenda.js
+++ api/models/encomenda.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose')
 const EncomendaSchema = new mongoose.Schema({
     id: {type: Number, required: true, unique: true},
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilizador' },
-    produtos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Produto'}]
+    produtos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Produto'}],
+    estado: {
+        type: String,
+        enum: ['Em Tratamento', 'Enviada para o cliente',
+                'Recebida pelo cliente', 'Cancelada'],
+        default: 'Em Tratamento',        
+    },
 })
 
 module.exports = mongoose.model('Encomenda', EncomendaSchema)
